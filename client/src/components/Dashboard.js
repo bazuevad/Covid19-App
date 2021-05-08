86c9a8d7dfd1aa011d27d1dd13e66d70cce605ee
hancong97 <31752800+hancong97@users.noreply.github.com>
2021-05-08 03:12:21 -0400

    Cong_dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -95,15 +95,32 @@ export default class Dashboard extends React.Component {
       <div className="Dashboard">
 
         <PageNavbar active="dashboard" />
-        <div class="container">
-					<img src={require('C:/Users/Cong Han/OneDrive/Desktop/CIS550/hw2/client/src/image/mina.jpg')} className="image"/>
+        <div Class="container1">
+					<img src={require('C:/Users/Cong Han/OneDrive/Desktop/CIS550/hw2/client/src/image/covid1.png')} className="covid1"/>
+          <div class="bottom-left">Bottom LeftCoronavirus disease 2019 (COVID-19), also known as the coronavirus, COVID or Covid, is a contagious disease caused
+           by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2).
+           The first known case was identified in Wuhan, China, in December 2019.The disease has since spread worldwide, leading to an ongoing pandemic.</div>
+		  		</div>
+          
+          <div class="container">
+					<img src={require('C:/Users/Cong Han/OneDrive/Desktop/CIS550/hw2/client/src/image/covid2.png')} className="covid2"/>
 						<div class="middle">
-							<a href="https://baidu.com">
-    							<div class="text">Mina</div>
+							<a href="https://www.cdc.gov/coronavirus/2019-ncov/index.html">
+    							<div class="text">Symptoms of COVID-19</div>
 							</a>
   						</div>
 		  		</div>
-          
+
+          <div class="container3">
+					<img src={require('C:/Users/Cong Han/OneDrive/Desktop/CIS550/hw2/client/src/image/covid3.jpg')} className="covid3"/>
+						<div class="overlay">
+							<a href="https://www.cdc.gov/">
+    							<div class="text3">News of COVID-19</div>
+							</a>
+  						</div>
+		  		</div>
+
+
         
       </div>
     );

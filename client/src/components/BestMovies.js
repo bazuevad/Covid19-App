4b597775bd6ffd9671ca6b9bfde8cecebd4910a9
andongl1997 <55897032+andongl1997@users.noreply.github.com>
2021-05-08 22:05:46 -0400

    Add files via upload
    covid groups

--- client/src/components/BestMovies.js
+++ client/src/components/BestMovies.js
@@ -9,154 +9,178 @@ export default class BestMovies extends React.Component {
 		super(props);
 
 		this.state = {
-			selectedDecade: "",
-			selectedGenre: "",
-			decades: [],
-			genres: [],
-			movies: []
+			selectedSex:"",
+			selectedAgegroup: "",
+			selectedRace: "",
+			sexes: [],
+			agegroups: [],
+			races: [],
+			results: []
 		};
 
 		this.submitDecadeGenre = this.submitDecadeGenre.bind(this);
-		this.handleDecadeChange = this.handleDecadeChange.bind(this);
-		this.handleGenreChange = this.handleGenreChange.bind(this);
+
+		this.handleSexChange = this.handleSexChange.bind(this);
+		this.handleAgegroupChange = this.handleAgegroupChange.bind(this);
+		this.handleRaceChange = this.handleRaceChange.bind(this);
 	};
 
 	/* ---- Q3a (Best Movies) ---- */
 	componentDidMount() {
-		// Send an HTTP request to the server.
-		
-		fetch("http://localhost:8081/decades",
-		
-		{
-		  method: 'GET' // The type of HTTP request.
-		}).then(res => {
-		  // Convert the response data to a JSON.
-		  return res.json();
-		}, err => {
-		  // Print the error if there is one.
-		  console.log(err);
-		}).then(decadesList => {
-		  if (!decadesList) return;
-		  const decadesDivs = decadesList.map((decadesObj,i) =>
-		  // Set the state of the keywords list to the value returned by the HTTP response from the server.
-		  <option
-				key = {i}
-				className = "decadesOption"
-				value = {decadesObj.Decade} > {decadesObj.Decade}
-			</option>
-		  );
-		  this.setState({
-			decades: decadesDivs,
-			selectedDecade: decadesDivs[0].props.value,
+
+		   this.setState({
+			sexes:  [{sex: "Male"}, {sex: "Female"}].map((sexesObj, i) =>
+  		  <option
+  		      key={i}
+  		      className="sexesOption"
+  			  value={sexesObj.sex}>{sexesObj.sex}
+  		  </option>
+  		  ),
+			selectedSex: 'Male',
 		  });
-		}, err => {
-		  // Print the error if there is one.
-		  console.log(err);
-		});
+
+		  this.setState({
+			agegroups: [{Decade: "0 - 9 Years"},
+  		                       {Decade: "10 - 19 Years"},
+  							   {Decade: "20 - 29 Years"},
+  							   {Decade: "30 - 39 Years"},
+  							   {Decade: "40 - 49 Years"},
+  							   {Decade: "50 - 59 Years"},
+  							   {Decade: "60 - 69 Years"},
+  							   {Decade: "70 - 79 Years"},
+  							   {Decade: "80+ Years"}].map((decadesObj, i) =>
+  		  <option
+  		      key={i}
+  		      className="decadesOption"
+  			  value={decadesObj.Decade}>{decadesObj.Decade}
+  		  </option>
+  		  ),
+			selectedAgegroup: "0 - 9 Years",
+		  });
 
 
-		fetch("http://localhost:8081/genres",
-		
-		{
-		  method: 'GET' // The type of HTTP request.
-		}).then(res => {
-		  // Convert the response data to a JSON.
-		  return res.json();
-		}, err => {
-		  // Print the error if there is one.
-		  console.log(err);
-		}).then(genresList => {
-		  if (!genresList) return;
-		  console.log(genresList);
-		  const genresDivs = genresList.map((genresObj,i) =>
-		  // Set the state of the keywords list to the value returned by the HTTP response from the server.
-		  <option
-				key = {i}
-				className = "genresOption"
-				value = {genresObj.name} > {genresObj.name}
-			</option>
-		  );
-		  console.log(genresDivs[0].props.value);
 		  this.setState({
-			genres: genresDivs,
-			selectedGenre: genresDivs[0].props.value,
+			races: [{Race: "American Indian-Alaska Native"},
+  		                     {Race: "Asian"},
+  							 {Race: "Black"},
+  							 {Race: "Hispanic-Latino"},
+  							 {Race: "Multiple-Other"},
+  							 {Race: "Native Hawaiian-Other Pacific Islander"},
+  							 {Race: "White"}].map((racesObj, i) =>
+  		  <option
+  		      key={i}
+  		      className="racesOption"
+  			  value={racesObj.Race}>{racesObj.Race}
+  		  </option>
+  		  ),
+			selectedRace: "American Indian-Alaska Native",
 		  });
-		}, err => {
-		  // Print the error if there is one.
-		  console.log(err);
-		});
+
+
 	};
 
 	/* ---- Q3a (Best Movies) ---- */
-	handleDecadeChange(e) {
+	handleSexChange(e) {
+		console.log(e.target.value);
 		this.setState({
-			selectedDecade: e.target.value
+			selectedSex: e.target.value
 		});
+
+
 	};
 
-	handleGenreChange(e) {
+	handleAgegroupChange(e) {
+		console.log(e.target.value);
 		this.setState({
-			selectedGenre: e.target.value
-			
+			selectedAgegroup: e.target.value
 		});
-		
+
+
+	};
+
+	handleRaceChange(e) {
+		console.log(e.target.value);
+		this.setState({
+			selectedRace: e.target.value
+		});
+
 	};
 
 	/* ---- Q3b (Best Movies) ---- */
 	submitDecadeGenre() {
-		// Send an HTTP request to the server.
-		fetch("http://localhost:8081/bestmovies/" + this.state.selectedDecade + "/" + this.state.selectedGenre,
+		fetch("http://localhost:8081/groups/"+ this.state.selectedSex +"/" +this.state.selectedAgegroup+"/"+this.state.selectedRace,
 		{
 		  method: 'GET' // The type of HTTP request.
-		}).then(res => {
-		  // Convert the response data to a JSON.
+		}).then(res =>{
 		  return res.json();
 		}, err => {
 		  // Print the error if there is one.
+		  console.log('nononononononono');
 		  console.log(err);
-		}).then(movieList => {
-			
-		  if (!movieList) return;
-		console.log(this.state.selectedGenre);
-			
-		  // Map each keyword in this.state.keywords to an HTML element:
-		  // A button which triggers the showMovies function for each keyword.
-		  
-		  const bestmoviesDivs = movieList.map((keywordObj, i) =>
-		  
+	  }).then(resList => {
+		  if (!resList) return;
+         console.log(resList);
+		  const resDivs = resList.map((resObj, i) =>
 			<BestMoviesRow
-			  title = {keywordObj.title}
-			  movie_id = {keywordObj.movie_id}
-			  rating = {keywordObj.rating}
-	
-			/> 
+			    key={i}
+				ratio1={resObj.ratio1}
+				ratio2={resObj.ratio2}
+				ratio3={resObj.ratio3}
+			/>
 		  );
+
 		  // Set the state of the keywords list to the value returned by the HTTP response from the server.
 		  this.setState({
-			movies: bestmoviesDivs
+			results:resDivs
 		  });
 		}, err => {
-		  // Print the error if there is one.
 		  console.log(err);
 		});
+
 	};
 
 	render() {
 		return (
 			<div className="BestMovies">
-				
-				<PageNavbar active="bestgenres" />
 
-			  	<div class="container">
-					<img src={require('C:/Users/Cong Han/OneDrive/Desktop/CIS550/hw2/client/src/image/mina.jpg')} className="image"/>
-						<div class="middle">
-							<a href="https://baidu.com">
-    							<div class="text">Mina</div>
-							</a>
-  						</div>
-		  		</div>
+				<PageNavbar active="groups" />
+
+				<div className="container bestmovies-container">
+					<div className="jumbotron">
+						<div className="h5">Covid Group Exploration</div>
+						<br></br>
+						<div className="h6">By choosing sex, race and age group, we can see three the percentage of whether people infected by <br></br>COVID-19 among their group are in hospital, in icu or death.
+                        </div>
+						<div className="dropdown-container">
+						    <select value={this.state.selectedSex} onChange={this.handleSexChange} className="dropdown" id="sexesDropdown">
+							    {this.state.sexes}
+						    </select>
+							<select value={this.state.selectedAgegroup} onChange={this.handleAgegroupChange} className="dropdown" id="agegroupsDropdown">
+								{this.state.agegroups}
+							</select>
+							<select value={this.state.selectedRace} onChange={this.handleRaceChange} className="dropdown" id="racesDropdown">
+								{this.state.races}
+							</select>
+							<button className="submit-btn" id="submitBtn" onClick={this.submitDecadeGenre}>Submit</button>
+						</div>
+					</div>
+					<div className="jumbotron">
+					<div className="h6">The result is shown below, in which 1 means people who are infected among the selected group are 100% in hospital/in icu/death<br></br>
+					and 0 means people who are infected among the selected group are 0% in hospital/in icu/death</div>
+					<br></br>
+						<div className="movies-container">
+							<div className="movie">
+								<div className="header"><strong>Ratio of In Hospitial</strong></div>
+								<div className="header"><strong>Ratio of In ICU</strong></div>
+								<div className="header"><strong>Ratio of Death</strong></div>
+					</div>
+			        <div className="movies-container" id="results">
+			          {this.state.results}
+			        </div>
+			      </div>
+			    </div>
+			  </div>
 			</div>
-			
 		);
 	};
 };
